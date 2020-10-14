5204f3da83821e2b28707aaa21f43b647bca33f0
yirleison <yppalomeque@gmail.com>
2020-10-14 16:22:05 -0500

    Se actualiza ruta para el build de la app

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/app-loans'));
+app.use(express.static(__dirname + '/dist/appLoans'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +
-    '/dist/app-loans/index.html'));
+    '/dist/appLoans/index.html'));
 });
 app.listen(process.env.PORT || 8080);
 console.log('Server runnig on port ', process.env.PORT || 8080);
