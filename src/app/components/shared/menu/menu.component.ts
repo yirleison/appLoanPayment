cb12f4f31942dfaeebb3b8e80a865ede0d36a1c7
yirleison <yppalomeque@gmail.com>
2020-10-15 14:35:02 -0500

    Se corrige src de foto del usuario

--- src/app/components/shared/menu/menu.component.ts
+++ src/app/components/shared/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { OauthService } from 'src/app/services/oauth-services/oauth.user.service';
+import { Endpoints } from '../../config/endpoints'
 declare var $;
 @Component({
   selector: 'app-menu',
@@ -11,16 +12,21 @@ declare var $;
 export class MenuComponent implements OnInit {
   public user: any
   name: String
+  public urlBase: String;
+  public urlPhoto: String
+  public avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQzr9MZuVHIBRWTXhdzLQAx_-Y0e5Wg6-MmJv4uLE1AyHnhdA5V&usqp=CAU'
 
   constructor(private _route: Router, private oauthService : OauthService) {
     this.user = JSON.parse(this.oauthService.getCurrenUser())
-
+    this.urlBase = Endpoints.url
   }
 
   ngOnInit() {
 
    this.name = this.user.fullName.split(' ')[0]
    console.log('show data user -------->',this.user.potho)
+
+   this.urlPhoto = ( this.user.potho == '' || this.user.potho == null ? this.avatar : this.urlBase + this.user.potho)
    // console.log('Usuario------>',name)
   //  $(document).ready(() => {
   //   const trees: any = $('[data-widget="tree"]');
